07ab116456823b92e9c8963eaca5a6ae648390ec
Kevin Chabowski <kevin@kch42.de>
2014-03-24 21:05:26 +0100

    Moved message stuff into own file

--- rooms.go
+++ rooms.go
@@ -1,36 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 )
-
-type MsgType int
-
-const (
-	MsgChat MsgType = iota // Default
-	MsgJoin
-	MsgLeave
-)
-
-func (mt *MsgType) MarshalJSON() ([]byte, error) {
-	switch *mt {
-	case MsgChat:
-		return json.Marshal("chat")
-	case MsgJoin:
-		return json.Marshal("join")
-	case MsgLeave:
-		return json.Marshal("leave")
-	}
-
-	return nil, errors.New("Unknown message type")
-}
-
-type Message struct {
-	Type MsgType `json:"type"`
-	User string  `json:"user"`
-	Text string  `json:"text,omitempty"`
-}
 
 type Room struct {
 	Messages chan Message
